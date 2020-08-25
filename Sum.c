764ce2c760105181d13dd27f93c51e2946c00eff
queen624 <70107967+queen624@users.noreply.github.com>
2020-08-25 15:19:30 +0530

    Sum.c
    This print the sum of numbers

---
+++ Sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+Int sum of digits(int n)
+{
+Int n==0
+Return 0:
+
+Return (n%10 + sum of digits(n/10));
+}
+Int main()
+{
+Int num=1234;
+Int result=sum of digits(num);
+Printf("sum of digits in %d is %d/n",n,result);
+Return 0;
+}
